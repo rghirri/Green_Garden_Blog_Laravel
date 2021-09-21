56fbf7f1c34d8a8f1dbb4ca6846e8c5cc2345dc1
Rayaa <rghirri@gmail.com>
2021-09-21 17:38:43 +0100

    Models and migrations creted. Update to app.blade.php

---
+++ database/migrations/2021_09_21_160902_create_articles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('content');
+            $table->timestamps('published_at')->nullable();
+            $table->string('image_file_list')->nullable();
+            $table->string('image_file_banner')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('articles');
+    }
+}
